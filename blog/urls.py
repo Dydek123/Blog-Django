49cc561b96e6b2bd631b61e3e785fa8d09c2b21a
Dydek123 <mazurkiewicz707@gmail.com>
2020-08-20 11:17:32 +0200

    Added option to add/update/delete posts.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
-
-# <app> / <model>_<viewtype>.html
