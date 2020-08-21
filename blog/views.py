8b76247d70f19433904b69ac4b4aa5e031d49b1c
Dydek123 <mazurkiewicz707@gmail.com>
2020-08-21 19:51:56 +0200

    Visual modifications.

--- blog/views.py
+++ blog/views.py
@@ -37,7 +37,7 @@ class PostDetailView(DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'heading', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -45,7 +45,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']
+    fields = ['title', 'heading', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
--- users/views.py
+++ users/views.py
@@ -3,7 +3,6 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
-# Create your views here.
 
 def register(request):
     if request.method == 'POST':
@@ -11,11 +10,12 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created!You are now able to log in!')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
 
 @login_required
 def profile(request):
@@ -27,7 +27,7 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, f'Your profile has been updated!')
+            messages.success(request, f'Your account has been updated!')
             return redirect('profile')
 
     else:
